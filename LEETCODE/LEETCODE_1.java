3f72024a27ec2d8df5c46bbb02ffae82f0a1ac18
Ryeong_Lee <33948776+ryeongs@users.noreply.github.com>
2021-12-16 17:49:05 +0900

    Update LEETCODE_1.java

--- LEETCODE/LEETCODE_1.java
+++ LEETCODE/LEETCODE_1.java
@@ -6,10 +6,8 @@ class Solution {
             if (map.containsKey(nums[i])) {
                 return new int[]{map.get(nums[i]), i};
             }
-
             map.put(target - nums[i], i);
         }
-
-        return new int[]{-1, -1};
+        return new int[]{};
     }
 }
