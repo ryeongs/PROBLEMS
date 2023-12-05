15d61051bc28301e74c47420e33495aa63cc929b
Ryeong_Lee <33948776+ryeongs@users.noreply.github.com>
2023-12-05 09:18:46 +0000

    Create 최댓값과 최솟값.java

---
+++ PROGRAMMERS/최댓값과 최솟값.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split(" ");
+        int[] num = new int[str.length];
+
+        for(int i = 0; i < str.length; i++) {
+            num[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(num);
+
+        answer = num[0] + " " + num[num.length-1];
+        return answer;
+    }
+}
